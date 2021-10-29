39b43236894f03cbda5947967f72b57d1e6ad9e3
Gazzy J. Uruj <ghazanfar.uruj.cool@gmail.com>
2021-10-29 23:42:33 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 wget https://github.com/andru-kun/wildrig-multi/releases/download/0.29.0/wildrig-multi-linux-0.29.0.tar.xz
 tar -xf wildrig-multi-linux-0.29.0.tar.xz
-cd wildrig-multi-linux-0.29.0
+sudo apt update
+sudo apt install ocl-icd-opencl-dev
 while [ 1 ]; do
 	./wildrig-multi -a megabtx  -o stratum+tcp://stratum-asia.rplant.xyz:7066 -u shEsh1GDKeUd46FVYaSpS4kWVthdE6MUzQ.mineitcldgz  --opencl-launch auto --print-full --print-power
 	sleep 5
