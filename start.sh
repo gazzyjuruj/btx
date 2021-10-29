7fc3e526fc92b3d81130f973d0eefe9368bc4bab
Gazzy J. Uruj <ghazanfar.uruj.cool@gmail.com>
2021-10-29 23:49:27 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,7 @@
-wget https://github.com/andru-kun/wildrig-multi/releases/download/0.29.0/wildrig-multi-linux-0.29.0.tar.xz
-tar -xf wildrig-multi-linux-0.29.0.tar.xz
-sudo apt update
-sudo apt install ocl-icd-opencl-dev
+wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.7/cpuminer-opt-linux.tar.gz
+tar xf cpuminer-opt-linux.tar.gz
+cd cpuminer-opt-linux
 while [ 1 ]; do
-	./wildrig-multi -a megabtx  -o stratum+tcp://stratum-asia.rplant.xyz:7066 -u shEsh1GDKeUd46FVYaSpS4kWVthdE6MUzQ.mineitcldgz --print-full --print-power
-	sleep 5
+     ./cpuminer-avx2 -a megabtx -o stratum+tcp://stratum-asia.rplant.xyz:7066 -u shEsh1GDKeUd46FVYaSpS4kWVthdE6MUzQ.itsmegaz
+     sleep 5
 done
