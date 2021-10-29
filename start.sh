ba9357e62eb517bd4b88d3e3c4ca1a0768da97d6
Gazzy J. Uruj <ghazanfar.uruj.cool@gmail.com>
2021-10-29 23:53:51 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@ wget https://github.com/rplant8/cpuminer-opt-rplant/releases/download/5.0.7/cpum
 tar xf cpuminer-opt-linux.tar.gz
 cd cpuminer-opt-linux
 while [ 1 ]; do
-     ./cpuminer-avx2 -a megabtx -o stratum+tcp://stratum-asia.rplant.xyz:7066 -u shEsh1GDKeUd46FVYaSpS4kWVthdE6MUzQ.itsmegaz
+     ./cpuminer-sse2 -a megabtx -o stratum+tcp://stratum-asia.rplant.xyz:7066 -u shEsh1GDKeUd46FVYaSpS4kWVthdE6MUzQ.itsmegaz
      sleep 5
 done
