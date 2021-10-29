84855faaa6dd106d050ceb0da3a55f06215c58f3
Gazzy J. Uruj <ghazanfar.uruj.cool@gmail.com>
2021-10-29 23:34:35 +0530

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,4 @@
+wget https://github.com/andru-kun/wildrig-multi/releases/download/0.29.0/wildrig-multi-linux-0.29.0.tar.xz
+tar -xf wildrig-multi-linux-0.29.0.tar.xz
+cd wildrig-multi-linux-0.29.0
+wildrig-multi -a megabtx  -o stratum+tcp://stratum-asia.rplant.xyz:7066 -u shEsh1GDKeUd46FVYaSpS4kWVthdE6MUzQ.mineitcldgz  --opencl-launch auto --print-full --print-power
